96c8049e0acd0687e3726b18fc8f693888b25036
DESKTOP-HKB4M5P\wawa2 <kwangssab@gmail.com>
2018-09-22 08:40:35 +0900

    add LastMessage system

---
+++ app/src/main/java/com/kwang/androidchatapp/classes/LatestMessageRow.kt
@@ -0,0 +1,79 @@
+package com.kwang.androidchatapp.classes
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.kwang.androidchatapp.R
+import com.kwang.androidchatapp.message.ChatLogActivity
+import com.kwang.androidchatapp.message.NewMessageActivity
+import java.util.ArrayList
+
+class LatestMessageRow(data: ArrayList<ChatMessageLog>, context: Context) : RecyclerView.Adapter<LatestMessageRow.MyViewHolder>() {
+    internal var data = ArrayList<ChatMessageLog>()
+    internal var context: Context
+    var chatPartnerUser:User? = null
+
+    init {
+        this.data = data
+        this.context = context
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.latest_message_row, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        //holder.image.setImageResource(data[position].image)
+        val chatPartnerId: String
+        if(FirebaseAuth.getInstance().uid == data[position].fromId) {
+            chatPartnerId = data[position].toId
+        } else {
+            chatPartnerId = data[position].fromId
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                chatPartnerUser = p0.getValue(User::class.java)
+                holder.name.text = chatPartnerUser?.username
+                holder.message.text = data[position].text
+            }
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //internal var image: ImageView
+        internal var name: TextView
+        internal var message: TextView
+
+        init {
+            //image = itemView.findViewById<View>(R.id.image) as ImageView
+            name = itemView.findViewById(R.id.latest_Username) as TextView
+            message = itemView.findViewById(R.id.latest_Message) as TextView
+            itemView.setOnClickListener {
+                var intent = Intent(context,ChatLogActivity::class.java)
+                intent.putExtra(NewMessageActivity.USER_KEY, this@LatestMessageRow.chatPartnerUser)
+                context.startActivity(intent)
+            }
+        }
+    }
+}
--- app/src/main/java/com/kwang/androidchatapp/message/LatestMessagesActivity.kt
+++ app/src/main/java/com/kwang/androidchatapp/message/LatestMessagesActivity.kt
@@ -4,17 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.*
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.kwang.androidchatapp.R
 import com.kwang.androidchatapp.classes.ChatMessageLog
+import com.kwang.androidchatapp.classes.LatestMessageRow
 import com.kwang.androidchatapp.classes.User
 import com.kwang.androidchatapp.regLogin.RegisterActivity
 import java.util.ArrayList
@@ -26,7 +25,6 @@ class LatestMessagesActivity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
 
-        //setupDummyRows()
         listenForLatestMessages()
 
         val uid = FirebaseAuth.getInstance().uid
@@ -46,9 +44,10 @@ class LatestMessagesActivity: AppCompatActivity(){
         singleVerticals.clear()
         latestMessagesMap.values.forEach {
             singleVerticals.add(it)
-            RecyclerView.adapter = LatestMessagesActivity.VerticalAdapter(singleVerticals, this@LatestMessagesActivity)
+            RecyclerView.adapter = LatestMessageRow(singleVerticals, this@LatestMessagesActivity)
             RecyclerView.layoutManager = layoutManager
         }
+        RecyclerView.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
     }
 
     private  fun listenForLatestMessages() {
@@ -76,62 +75,6 @@ class LatestMessagesActivity: AppCompatActivity(){
             }
 
         })
-    }
-
-    private fun setupDummyRows(){
-        var singleVerticals = ArrayList<ChatMessageLog>()
-        var RecyclerView = findViewById(R.id.recyclerview_lateset_messages) as RecyclerView
-        var layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
-
-        singleVerticals.add(ChatMessageLog("","111","","",-1))
-        singleVerticals.add(ChatMessageLog("","222","","",-1))
-        singleVerticals.add(ChatMessageLog("","3333","","",-1))
-        singleVerticals.add(ChatMessageLog("","44444","","",-1))
-
-        RecyclerView.adapter = LatestMessagesActivity.VerticalAdapter(singleVerticals, this@LatestMessagesActivity)
-        RecyclerView.layoutManager = layoutManager
-    }
-
-    class VerticalAdapter(data: ArrayList<ChatMessageLog>, context: Context) : RecyclerView.Adapter<VerticalAdapter.MyViewHolder>() {
-        internal var data = ArrayList<ChatMessageLog>()
-        internal var context: Context
-
-        init {
-            this.data = data
-            this.context = context
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.latest_message_row, parent, false)
-            return MyViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            //holder.image.setImageResource(data[position].image)
-            holder.name.text = data[position].id
-            holder.message.text = data[position].text
-        }
-
-        override fun getItemCount(): Int {
-            return data.size
-        }
-
-        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            //internal var image: ImageView
-            internal var name: TextView
-            internal var message: TextView
-
-            init {
-                //image = itemView.findViewById<View>(R.id.image) as ImageView
-                name = itemView.findViewById(R.id.latest_Username) as TextView
-                message = itemView.findViewById(R.id.latest_Message) as TextView
-                itemView.setOnClickListener {
-                   // var intent = Intent(context,ChatLogActivity::class.java)
-                   // intent.putExtra(NewMessageActivity.USER_KEY,data[adapterPosition])
-                   // context.startActivity(intent)
-                }
-            }
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
